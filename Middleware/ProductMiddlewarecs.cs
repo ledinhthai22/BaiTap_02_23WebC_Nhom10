06469c72f2c3e587acd38df6c1b4c261793a13d2
Le Dinh Thai <ledinhthai0222@gmail.com>
2025-09-24 20:40:55 +0700

    Write data into db.json

--- Middleware/ProductMiddlewarecs.cs
+++ Middleware/ProductMiddlewarecs.cs
@@ -4,29 +4,9 @@ using Microsoft.AspNetCore.Http;
 
 namespace BaiTap_02_23WebC_Nhom10.Middleware
 {
-    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+   
     public class ProductMiddlewarecs
     {
-        private readonly RequestDelegate _next;
 
-        public ProductMiddlewarecs(RequestDelegate next)
-        {
-            _next = next;
-        }
-
-        public Task Invoke(HttpContext httpContext)
-        {
-
-            return _next(httpContext);
-        }
-    }
-
-    // Extension method used to add the middleware to the HTTP request pipeline.
-    public static class ProductMiddlewarecsExtensions
-    {
-        public static IApplicationBuilder UseProductMiddlewarecs(this IApplicationBuilder builder)
-        {
-            return builder.UseMiddleware<ProductMiddlewarecs>();
-        }
     }
 }
