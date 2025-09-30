5998529d6d4f6da52dfb34082b10dcb8bc516c82
khclng <kgming107@gmail.com>
2025-09-30 07:40:21 +0700

    create-product-ver2

--- Areas/Admin/Controllers/ProductsController.cs
+++ Areas/Admin/Controllers/ProductsController.cs
@@ -4,11 +4,17 @@ namespace BaiTap_02_23WebC_Nhom10.Areas.Admin.Controllers
 {
     public class ProductsController : Controller
     {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ProductsController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
         [Area("Admin")]
         public IActionResult Index()
         {
             return View();
         }
+        [Area("Admin")]
         public IActionResult Create()
         {
             return View();
