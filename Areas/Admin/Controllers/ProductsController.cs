72446e915fb38774e3974f78cfcb5801e2e42f2c
khclng <kgming107@gmail.com>
2025-10-01 14:50:11 +0700

    refactor-project & pagin-page-product

--- Areas/Admin/Controllers/ProductsController.cs
+++ Areas/Admin/Controllers/ProductsController.cs
@@ -1,10 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿// Bổ sung đầy đủ các using cần thiết ở đầu file
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using BaiTap_02_23WebC_Nhom10.Models;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http; // Cần thiết cho IFormFile
 
 namespace BaiTap_02_23WebC_Nhom10.Areas.Admin.Controllers
 {
     public class ProductsController : Controller
     {
         private readonly IWebHostEnvironment _webHostEnvironment;
+
         public ProductsController(IWebHostEnvironment webHostEnvironment)
         {
             _webHostEnvironment = webHostEnvironment;
@@ -19,5 +30,6 @@ namespace BaiTap_02_23WebC_Nhom10.Areas.Admin.Controllers
         {
             return View();
         }
+        
     }
 }
--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,17 +1,22 @@
 ﻿using BaiTap_02_23WebC_Nhom10.Models;
 using Microsoft.AspNetCore.Mvc;
 using BaiTap_02_23WebC_Nhom10.Service;
+using PagedList;
 
 namespace WebApplication1.Controllers
 {
     public class ProductController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(int page = 1, int pageSize = 8)
         {
             if (HttpContext.Items.TryGetValue("product", out var productServiceObj) && productServiceObj is ProductService productService)
             {
-                var product = productService.Products;
-                return View(product);
+                var product = productService.Products.ToList();
+                var pagedProduct = product.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+                ViewBag.CurrentPage = page;
+                ViewBag.TotalItems = product.Count;
+                ViewBag.PageSize = pageSize;
+                return View(pagedProduct);
             }
             return View("~/Views/Home/NotFound");
         }
